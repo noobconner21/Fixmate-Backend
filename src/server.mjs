1dff156c337fbb5b05a3335eae839cec4e22aa94
dumiduzee <dumidulk@outlook.com>
2025-06-17 15:36:24 +0530

    enhansed response message and implement user router

--- src/server.mjs
+++ src/server.mjs
@@ -3,6 +3,7 @@ import ENV from "./utils/ENV.mjs"
 import cors from "cors"
 import auth_router from "./routes/auth.route.mjs"
 import { GlobalMiddleware } from "./middlewares/GlobalMiddleware.mjs"
+import user_router from "./routes/user.route.mjs"
 
 
 
@@ -16,10 +17,11 @@ app.use(express.json())
 
 //Routes
 app.use("/api/v1/auth",auth_router)
-
+app.use("/api/v1/user",user_router)
 
 app.use(GlobalMiddleware)
 
 app.listen(ENV.PORT,()=>{
     console.log(`Server is running🔥 on http://localhost:${ENV.PORT}`);
 })
+
